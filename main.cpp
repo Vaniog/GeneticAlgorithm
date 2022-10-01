a23457f41668c49900b3570c024a57ea1b645bd5
Vaniog <forvantar@gmail.com>
2022-10-01 14:38:40 +0300

    submodule remade

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include <cmath>
+#include "GraphicsManager/Space.h"
 
-int main(){
-    std::cout << "Hello world! I am here.";
+class Circle : public Object {
+    void OnDraw(sf::RenderWindow& window) override {
+        sf::CircleShape circle_shape;
+        circle_shape.setRadius(150);
+        circle_shape.setPosition(window_width / 2 + 200 * cos((double)time / 10000),
+                                 window_height / 2 + 200 * sin((double)time / 10000));
+        window.draw(circle_shape);
+    }
+};
+
+int main() {
+    Space space;
+    space << new Circle;
+    space.Start();
 }
