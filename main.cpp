8f0c32deb42f96523bb75850604151d0d6903921
EgorFarafonov367578 <367578@edu.itmo.ru>
2022-10-05 21:54:41 +0300

    some changes

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include "GraphicsManager/Space.h"
 #include "SpaceManager.h"
+#include "GeneticAlgorithmCore/Evolution.h"
 
 int main() {
     Space space;
     SpaceManager space_manager(space);
     space_manager.FillByTag(SpaceManager::Mode::MENU);
     space.Start();
-    evolution("a.txt","b.txt");
+    Evolution("a.txt","b.txt",10);
 }
