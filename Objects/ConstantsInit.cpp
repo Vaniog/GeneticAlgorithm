3163666a2978d956e72ea34819b9094c078ead08
Vaniog <forvantar@gmail.com>
2022-10-08 20:40:51 +0300

    scene stats

--- Objects/ConstantsInit.cpp
+++ Objects/ConstantsInit.cpp
@@ -14,14 +14,17 @@ const int SceneDraft::FONT_BORDER = 2;
 
 const std::string SceneGraphic::OLD_GEN_FILENAME = "../Resources/a.txt";
 const std::string SceneGraphic::NEW_GEN_FILENAME = "../Resources/a.txt";
-const int SceneGraphic::IMAGE_SIZE = 16;
+const int32_t SceneGraphic::IMAGE_SIZE = 16;
 const std::string SceneGraphic::FIELD_PATH = "../Resources/Field.png";
 const std::string SceneGraphic::FOOD_PATH = "../Resources/Food.png";
 const std::string SceneGraphic::BEAST_PATH = "../Resources/Beast.png";
 const std::string SceneGraphic::POISON_PATH = "../Resources/Poison.png";
 const std::string SceneGraphic::WALL_PATH = "../Resources/Wall.png";
-const std::string SceneGraphic::FONT_PATH = "../Resources/Font.ttf";
-const int SceneGraphic::FONT_BORDER = 2;
+const std::string SceneGraphic::HEALTH_FONT_PATH = "../Resources/HealthFont.ttf";
+const std::string SceneGraphic::STATS_FONT_PATH = "../Resources/StatsFont.ttf";
+const int32_t SceneGraphic::FONT_BORDER = 2;
+const int32_t SceneGraphic::MAX_STATS_SIZE = 15;
+const float SceneGraphic::STATS_FONT_SIZE = 1.5; // in tiles
 
 const std::string ExitButton::IMAGE_PATH = "../Resources/ExitButton.png";
 const std::string StartButton::IMAGE_PATH = "../Resources/StartButton.png";
