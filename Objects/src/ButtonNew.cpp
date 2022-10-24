ab207223f6b5dd3e5a281b5ae7931803fb02fbe1
Vaniog <forvantar@gmail.com>
2022-10-24 11:33:27 +0300

    better rectangle drawing

--- Objects/src/ButtonNew.cpp
+++ Objects/src/ButtonNew.cpp
@@ -71,10 +71,16 @@ bool IsSkipChar(const char& c) {
 }
 
 uint32_t ButtonData::GetColorFromString(const std::string& str) {
-    if (str[0] == '0') {
-        if (str.size() == 8) // if alpha not set
-            return static_cast<uint32_t>(std::stold(str + "FF"));
-        return static_cast<uint32_t>(std::stold(str));
+    std::string str_copy = str;
+    if (str[0] == '0' || str[0] == '#') {
+        if (str[0] == '#') { // #fafafa -> 0xfafafa
+            str_copy[0] = 'x';
+            str_copy = '0' + str_copy;
+        }
+        if (str_copy.size() == 8) { // 0xfafafa -> 0xfafafaff (alpha canal added)
+            str_copy += "FF";
+        }
+        return static_cast<uint32_t>(std::stold(str_copy));
     }
     if (str == "white") {
         return 0xFFFFFFFF;
@@ -92,7 +98,8 @@ uint32_t ButtonData::GetColorFromString(const std::string& str) {
         return 0xFF00FFFF;
     } else if (str == "cyan") {
         return 0x00FFFFFF;
-    }
+    } else if (str == "transparent")
+        return 0x00000000;
     return 0xFFFFFFFF;
 }
 
@@ -160,7 +167,9 @@ NewButton::NewButton(
         height = width * image_height / image_width;
         sprite->setTexture(*texture);
     } else {
-        rectangle = new sf::RectangleShape(sf::Vector2f(width, height));
+        rect_width = width - data.outline_thickness_in_pixels * 2;
+        rect_height = height - data.outline_thickness_in_pixels * 2;
+        rectangle = new sf::RectangleShape(sf::Vector2f(rect_width, rect_height));
         rectangle->setOutlineColor(sf::Color(data.outline_color));
         rectangle->setFillColor(sf::Color(data.fill_color));
         rectangle->setOutlineThickness(data.outline_thickness_in_pixels);
@@ -185,16 +194,18 @@ NewButton::NewButton(
 
 void NewButton::CorrectSize() const {
     if (image_defined) {
-        sprite->setPosition(pos_x - width * scale / 2, pos_y - height * scale / 2);
         sprite->setScale(size_scale * scale, size_scale * scale);
+        sprite->setPosition(pos_x - width * scale / 2, pos_y - height * scale / 2);
     } else {
-        rectangle->setPosition(pos_x - width * scale / 2, pos_y - height * scale / 2);
         rectangle->setScale(scale, scale);
+        rectangle->setPosition(pos_x - rect_width * scale / 2,
+                               pos_y - rect_height * scale / 2);
     }
     if (text_defined) {
-        text->setPosition(pos_x - text->getLocalBounds().width * scale / 2,
-                          pos_y - text->getLocalBounds().height * scale / 2);
         text->setScale(scale, scale);
+        float padding_y = data.character_size_in_pixels * scale - text->getGlobalBounds().height;
+        text->setPosition(pos_x - text->getGlobalBounds().width / 2,
+                          pos_y - text->getGlobalBounds().height / 2 - padding_y);
     }
 }
 
@@ -217,8 +228,10 @@ void NewButton::OnDraw(sf::RenderWindow& window) {
 void NewButton::OnEvent(sf::Event& event, sf::RenderWindow& window) {
     if (event.type == sf::Event::MouseMoved || event.type == sf::Event::MouseButtonPressed) {
         sf::Vector2<float> mouse = static_cast<sf::Vector2f>(sf::Mouse::getPosition(window));
-        if (mouse.x >= pos_x - width / 2 && mouse.x <= pos_x + width / 2
-                && mouse.y >= pos_y - height / 2 && mouse.y <= pos_y + height / 2) {
+        if (mouse.x >= pos_x - width / 2
+                && mouse.x <= pos_x + width / 2
+                && mouse.y >= pos_y - height / 2
+                && mouse.y <= pos_y + height / 2) {
             if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
                 scale = 0.85;
                 was_pressed = true;
