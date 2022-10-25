2df808b8c0130d682fe5e066ff0ff5094ec67c17
Vaniog <forvantar@gmail.com>
2022-10-25 11:05:34 +0300

    colors changed

--- SpaceManager.cpp
+++ SpaceManager.cpp
@@ -12,24 +12,24 @@ void SpaceManager::FillByTag(const SpaceManager::Mode mode) {
             space << new Background();
             space << new ExitButton("width:6% height:3% "
                                    "pos_x:5%  pos_y:3% "
-                                   "out_color:#ff69b4  fill_color:#ffffffef  out_thick:0.3% "
+                                   "out_color:#ca3299  fill_color:#ffffffef  out_thick:0.3% "
                                    "text_string:'exit'  char_size:2.5% "
                                    "font_path:'../Resources/Fonts/statsFont.ttf' "
-                                   "text_color:#ff69b4 ", space);
+                                   "text_color:#ca3299 ", space);
             space << new StartButton("width:25% height:10% "
                                     "pos_x:50%  pos_y:25% "
-                                    "out_color:#ff69b4  fill_color:#ffffffef  out_thick:1% "
+                                    "out_color:#ca3299  fill_color:#ffffffef  out_thick:1% "
                                     "text_string:'play'  char_size:8% "
                                     "font_path:'../Resources/Fonts/statsFont.ttf' "
-                                    "text_color:#ff69b4 ", *this);
+                                    "text_color:#ca3299 ", *this);
             break;
         case Mode::PLAY:
             space << new MenuButton("width:6% height:3% "
                                     "pos_x:5%  pos_y:3% "
-                                    "out_color:#ff69b4  fill_color:#ffffffef  out_thick:0.3% "
+                                    "out_color:#ca3299  fill_color:#ffffffef  out_thick:0.3% "
                                     "text_string:'menu'  char_size:2.5% "
                                     "font_path:'../Resources/Fonts/statsFont.ttf' "
-                                    "text_color:#ff69b4 ", *this);
+                                    "text_color:#ca3299 ", *this);
             space << new SceneGraphic(0.5, 0.5, 0.5);
             break;
     }
